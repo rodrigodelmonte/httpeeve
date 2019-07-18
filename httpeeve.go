46d1978375a87bfaa547132052a74c7e2fdc578a
Jamie Patel <jamie@notespublication.com>
2019-07-18 17:42:13 +0200

    Docs and helpers

--- httpeeve.go
+++ httpeeve.go
@@ -2,6 +2,7 @@ package httpeeve
 
 import (
 	"context"
+	"fmt"
 	"net"
 	"net/http"
 	"strings"
@@ -11,13 +12,15 @@ import (
 )
 
 type (
+	// Client represents an HTTP client. The "net/http".Client implements it for easy substitution in your project.
 	Client interface {
 		Do(*http.Request) (*http.Response, error)
 	}
 
 	clientFunc func(*http.Request) (*http.Response, error)
 
-	backoffConditioner func(resp *http.Response) (shouldRetry bool, err error)
+	// Conditioner determines whether a response is erroneous and whether to retry it.
+	Conditioner func(resp *http.Response) (shouldRetry bool, err error)
 
 	contextKeyAttempts struct{}
 )
@@ -26,7 +29,10 @@ func (c clientFunc) Do(req *http.Request) (*http.Response, error) {
 	return c(req)
 }
 
-func NewBackoffClient(httpClient http.Client, backoffer backoff.BackOff, conditioner backoffConditioner) Client {
+// NewBackoffClient returns a Client implementation. It takes an implementation of backoff.Backoff,
+// which determines the rate and limits of retrying. It takes a Conditioner which determines when to
+// stop or continue retrying.
+func NewBackoffClient(httpClient http.Client, backoffer backoff.BackOff, conditioner Conditioner) Client {
 	return clientFunc(func(req *http.Request) (*http.Response, error) {
 		var resp *http.Response
 		var attempts int
@@ -77,21 +83,47 @@ func categorizeRequestError(reqErr error) error {
 	}
 }
 
+// OK signals that no error occurred and we do not need to retry
+func OK() (bool, error) {
+	return false, nil
+}
+
+// RetriableError signals that a retriable error ocurred
+func RetriableError(msg string) (bool, error) {
+	return true, errors.New(msg)
+}
+
+// RetriableErrorf is like RetriableError but allows string formatting
+func RetriableErrorf(msg string, values ...interface{}) (bool, error) {
+	return true, fmt.Errorf(msg, values...)
+}
+
+// PermanentError signals that an error occurred that cannot be retried
+func PermanentError(msg string) (bool, error) {
+	return false, errors.New(msg)
+}
+
+// PermanentErrorf is like PermanentError but allows string formatting
+func PermanentErrorf(msg string, values ...interface{}) (bool, error) {
+	return false, fmt.Errorf(msg, values...)
+}
+
 // NewDefaultBackoffClient5XX retries
 func NewDefaultBackoffClient5XX(httpClient http.Client) Client {
 	return NewBackoffClient(httpClient, backoff.NewExponentialBackOff(), func(resp *http.Response) (bool, error) {
 		if resp.StatusCode >= 500 && resp.StatusCode < 600 {
-			return true, errors.Errorf("bad status code %d", resp.StatusCode)
+			return RetriableErrorf("bad status code %d", resp.StatusCode)
 		}
 
 		if resp.StatusCode >= 200 && resp.StatusCode < 300 {
-			return false, nil
+			return OK()
 		}
 
-		return false, errors.Errorf("bad status code %d", resp.StatusCode)
+		return PermanentErrorf("bad status code %d", resp.StatusCode)
 	})
 }
 
+// Attempts can be used to tell how many attempts a response took for its execution.
 func Attempts(resp *http.Response) int {
 	attempts, _ := resp.Request.Context().Value(contextKeyAttempts{}).(int)
 	return attempts
